9375ccd0e5e0432e21aecf05379e2a6da956ebae
Perrine GERNEZ <pgernez@e1r2p15.42.fr>
2017-01-11 22:21:23 +0100

    strchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,11 +6,12 @@
 /*   By: pgernez <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/08 18:16:50 by pgernez           #+#    #+#             */
-/*   Updated: 2017/01/11 21:23:12 by pgernez          ###   ########.fr       */
+/*   Updated: 2017/01/11 22:20:45 by pgernez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
 char	*ft_strchr(const char *s, int c)
 {
@@ -23,10 +24,14 @@ char	*ft_strchr(const char *s, int c)
 	s_tmp = (char*)s;
 	while (s[i])
 	{
-		if (s_tmp[i] != c_tmp)
-			i++;
-		else
+		if (s_tmp[i] == c_tmp)
 			return ((char*)&s[i]);
+		else
+			i++;
 	}
+	if (c_tmp == '\0')
+		return ((char*)&s[i]);
+	else
+		return (0);
 	return (0);
 }
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: pgernez <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/10 20:53:54 by pgernez           #+#    #+#             */
-/*   Updated: 2017/01/11 21:30:45 by pgernez          ###   ########.fr       */
+/*   Updated: 2017/01/11 22:19:53 by pgernez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,8 +41,8 @@ int	main(int ac, char **av)
 	
 	if (ac == 3)
 	{
-		str = (char*)strchr(av[1], '\0');
-		str2 = (char*)ft_strchr(av[2], '\0');
+		str = (char*)strchr(av[1], 'j');
+		str2 = (char*)ft_strchr(av[2], 'j');
 		printf("ptr est %p\n", str);
 		printf("ptr2 est %p\n", str2);
 		printf("str est %s\n", str);
