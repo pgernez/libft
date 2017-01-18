58f7906743b5b3d97ab00532f9fd5d99fb40b032
Perrine GERNEZ <pgernez@e3r6p5.42.fr>
2017-01-18 23:19:58 +0100

    strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: pgernez <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/15 23:02:20 by pgernez           #+#    #+#             */
-/*   Updated: 2017/01/15 23:41:09 by pgernez          ###   ########.fr       */
+/*   Updated: 2017/01/18 23:19:19 by pgernez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,24 @@ char	*ft_strtrim(char const *s)
 {
 	size_t	i;
 	size_t	j;
+	size_t	k;
 	char	*trim;
+	char	start;
 
 	i = 0;
 	j = 0;
-	trim = (char*)malloc(sizeof(size_t) * ft_strlen(s));
-	{
-		if (trim == 0)
-			return (NULL);
-	}
-	if (s[j] != ' ' && s[j] != '\t' && s[j] != '\n')
-		trim[i++] = s[j++];
-	else
-		j++;
-	if (s[j++] != ' ' && s[j++] != '\n' && s[j++] != '\t')
+	k = (ft_strlen(s) - 1);
+	if (s[j] == 0)
 		return ((char*)s);
-	else
-		return (trim);
+	if (!(trim = (char*)malloc(sizeof(size_t) * ft_strlen(s))))
+		return (NULL);
+	while (s[j] == ' ' || s[j] == '\n' || s[j] == '\t')
+		j++;
+	start = j;
+	while (s[k] == ' ' || s[k] == '\n' || s[k] == '\t')
+		k--;
+	while (i <= (k - start))
+		trim[i++] = s[j++];
+	trim[i] = 0;
+	return (trim);
 }
